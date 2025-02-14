1fd365d4a12cd85fafc4720807abff37b2459304
raghad155 <159728244+raghad155@users.noreply.github.com>
2025-02-15 01:03:14 +0300

    lab2

--- CreditCard.java
+++ CreditCard.java
@@ -1,53 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
 
-package lab1;
-
-public class CreditCard {
-    private String customer;
-    private String bank;
-    private String account;
-    private int balance;
-    private int limit;
-
-    public CreditCard(String cust, String bk, String ac, int b, int lim) {
-        customer = cust;
-        bank = bk;
-        account = ac;
-        balance = b;
-        limit = lim;
-    }
-
-    public String getCustomer() {
-        return customer;
-    }
-
-    public String getBank() {
-        return bank;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public int   getBalance() {
-        return balance;
-    }
-
-    public int getLimit() {
-        return limit;
-    }
-
-    public boolean charge(int price) {
-        if (price + balance > limit)
-            return false;
-        balance += price;
-        return true;
-    }
-
-    public void makePayment(int payment) {
-        balance -= payment;
-    }
-
-    public void updateLimit(int newLimit) {
-        limit = newLimit;
-    }
+/**
+ *
+ * @author admin1
+ */
+class CreditCard {
+    
 }
